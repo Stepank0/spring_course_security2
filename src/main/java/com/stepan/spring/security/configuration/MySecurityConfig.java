7143b02f1737796b28a9fcc7004ffcdd1e280487
Stepan Koryagin <stepanswim@gmail.com>
2023-03-11 18:35:56 +0200

    update access for roles

--- src/main/java/com/stepan/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/stepan/spring/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.stepan.spring.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -11,11 +12,22 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")  // /** достуб и к дальнейшим страницам
+                .and().formLogin().permitAll(); //форма логина и пароля будет запрашиваться у всех
+
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         UserBuilder userBuilder = User.withDefaultPasswordEncoder();
         auth.inMemoryAuthentication()
                 .withUser(userBuilder.username("stepan").password("stepan").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("zaur").password("zaur").roles("HR"))
-                .withUser(userBuilder.username("olga").password("olga").roles("MAMAGER", "HR"));
+                .withUser(userBuilder.username("olga").password("olga").roles("MANAGER", "HR"));
+        // запрет для разных ролей на переход на определённый адресс
     }
 }
--- src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
+++ src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <%--
   Created by IntelliJ IDEA.
   User: stepa
@@ -13,12 +14,15 @@
 <body>
 <h3>Information for all employees</h3>
 <br><br>
-
+<security:authorize access="hasRole('HR')"> <%-- запрет видеть эту кнопку другим ролям --%>
 <input type="button" value="Salary" onclick="window.location.href='hr_info'">
 Only for HR staff
+</security:authorize>
 <br><br>
+<security:authorize access="hasRole('MANAGER')">
 <input type="button" value="Performance" onclick="window.location.href='manager_info'">
 Only for Managers
+</security:authorize>
 <br><br>
 
 </body>
